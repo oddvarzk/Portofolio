a22b857d1dfee5bd21b01245ac7acf5c63b7d126
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-10 03:26:50 +0100

    Home routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./layout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </div>
   );
