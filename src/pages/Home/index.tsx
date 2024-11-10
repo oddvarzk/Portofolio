a834e3e50dd99af790df5bc74935d39c7886f3fc
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-10 13:17:15 +0100

    Home skeleton readjusted for img

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,21 @@
 export function Home() {
   return (
-    <div className="flex justify-center text-white gap-10">
-      <div>
-        <h1 className="text-white font-Montserrat text-2xl">
-          Welcome to my Portofolio
-        </h1>
-        <p className="font-Montserrat text-center">
-          Generic Informational Text
-        </p>
+    <div className="mt-5">
+      <div className="flex justify-center text-Cream gap-10">
+        <div>
+          <h1 className="font-Montserrat text-2xl">Welcome to my Portofolio</h1>
+          <p className="font-Montserrat text-center">
+            Generic Informational Text
+          </p>
+        </div>
+        <div className="font-Montserrat">
+          <p>Image</p>
+        </div>
       </div>
-      <div className="font-Montserrat">
-        <p>Image</p>
+      <div className="flex justify-center">
+        <button className="bg-Cream text-black px-3 py-1 mt-5">
+          See more projects
+        </button>
       </div>
     </div>
   );
