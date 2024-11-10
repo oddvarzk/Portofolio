3d4330fccb8c49a0a951ef4bdda35b42c33536f8
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-10 03:26:56 +0100

    Home skeleton made

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+export function Home() {
+  return (
+    <div className="flex justify-center">
+      <div>
+        <h1 className="text-white font-Montserrat text-2xl">
+          Welcome to my Portofolio
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
