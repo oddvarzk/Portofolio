b3a41914083dfb65f9bf0e50cc1e4923147ad7f5
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-10 12:57:13 +0100

    Skeleton of Home Page made

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,16 @@
 export function Home() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center text-white gap-10">
       <div>
         <h1 className="text-white font-Montserrat text-2xl">
           Welcome to my Portofolio
         </h1>
+        <p className="font-Montserrat text-center">
+          Generic Informational Text
+        </p>
+      </div>
+      <div className="font-Montserrat">
+        <p>Image</p>
       </div>
     </div>
   );
