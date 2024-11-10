aec5591d8c4e96ab10451d8b9074b4f792123fea
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-10 02:11:34 +0100

    Routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-export function App() {
-  return <div className="text-center">Start</div>;
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./layout";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
