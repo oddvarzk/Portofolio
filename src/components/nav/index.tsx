105c593020cb0304cc9f38640dad9e360ad35864
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-10 02:11:28 +0100

    Nav added

---
+++ src/components/nav/index.tsx
@@ -0,0 +1,14 @@
+export function Nav() {
+  return (
+    <nav className="flex items-center">
+      <ul className="text-white flex font-Montserrat">
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Contact</li>
+        <li>About me</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
