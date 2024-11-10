ff897e883424bfdf6a0194e8b62957b955b41f3b
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-10 02:10:47 +0100

    Fonts added and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Cream: "#F8F4E3",
+        Charcoal: "#1a1a1a",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"], // for body text
+        Playfair: ["Playfair Display", "serif"], // for headings
+      },
+    },
   },
   plugins: [],
 };
