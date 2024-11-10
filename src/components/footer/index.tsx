1950a6038253ead886067e613a2e845743c9a0ac
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-10 03:06:30 +0100

    social icons implemented

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,13 +1,16 @@
 import logo from "../../assets/logo.png";
+import linkedin from "../../assets/linkedin.svg";
+import github from "../../assets/github.png";
 
 export function Footer() {
   return (
-    <footer className="flex justify-center border-t-2 border-t-neutral-700">
+    <footer className="flex justify-center border-t-2 border-t-neutral-700 gap-5">
       <div>
         <img src={logo} className="h-20"></img>
       </div>
-      <div className="flex items-center justify-center text-Cream font-Montserrat">
-        Other contact social media icons, plus contact
+      <div className="flex items-center justify-center text-Cream font-Montserrat text-sm gap-5">
+        <img src={linkedin} className="bg-Cream h-8" />
+        <img src={github} className="h-8" />
       </div>
     </footer>
   );
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,7 +1,7 @@
 export function Nav() {
   return (
     <nav className="flex items-center">
-      <ul className="text-white flex font-Montserrat">
+      <ul className="text-white flex font-Montserrat px-5 gap-20">
         <li>Home</li>
         <li>Projects</li>
         <li>Contact</li>
