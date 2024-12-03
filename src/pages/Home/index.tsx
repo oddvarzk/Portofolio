bc85ff60794299c11af0cd534d5bc4ca95259e5f
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-12-04 00:07:37 +0100

    home sections altered.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,24 @@
 export function Home() {
   return (
     <div className="mt-5">
-      <div className="flex justify-center text-Cream gap-10">
+      <div className="flex justify-center text-Cream gap-10 font-Montserrat">
         <div>
-          <h1 className="font-Montserrat text-2xl">Welcome to my Portofolio</h1>
-          <p className="font-Montserrat text-center">
-            Generic Informational Text
+          <h1 className="text-2xl">Hi!</h1>
+          <h2 className="text-lg py-2">Welcome to my Portofolio</h2>
+          <p className="w-1/2">
+            I'm a newly graduated Front-end Developer, this site is made so you
+            can get to know me better, but also see what skills i have to offer
+            with the wide range of projects i have created.
           </p>
+          <div className="flex">
+            <button className="bg-Cream text-black px-3 py-1 mt-5">
+              See more projects
+            </button>
+          </div>
         </div>
         <div className="font-Montserrat">
           <p>Image</p>
         </div>
-      </div>
-      <div className="flex justify-center">
-        <button className="bg-Cream text-black px-3 py-1 mt-5">
-          See more projects
-        </button>
       </div>
     </div>
   );
