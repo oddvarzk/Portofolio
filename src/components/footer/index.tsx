43e9697d8d46b1e18768e53d1642d536121e595a
oddvar zakarias kristiansen <oddvarzk@gmail.com>
2024-12-04 03:10:15 +0100

    footer placement readjusted

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import github from "../../assets/github.png";
 
 export function Footer() {
   return (
-    <footer className="flex flex-col items-center border-t-2 border-t-neutral-700 py-5 gap-4 bg-neutral-900 text-Cream font-Montserrat">
+    <footer className="flex md:flex-row flex-col items-center md:justify-between justify-between border-t-2 border-t-neutral-700 py-5 gap-4 bg-neutral-900 text-Cream font-Montserrat px-5">
       {/* Logo Section */}
       <div className="flex items-center gap-5">
         <img src={logo} className="h-20" alt="Logo" />
@@ -14,22 +14,7 @@ export function Footer() {
             reserved.
           </p>
         </div>
-      </div>
-
-      {/* Contact Information */}
-      <div className="flex flex-col items-center gap-2 text-center font-Montserrat">
-        <p className=" text-md">Contact me:</p>
-        <a
-          href="mailto:your-email@example.com"
-          className="hover:underline text-sm"
-        >
-          oddvarzk@gmail.com
-        </a>
-        <p className="font-Montserrat text-sm">Phone: +47 472 07 143</p>
-      </div>
-
-      {/* Social Media Links */}
-      <div className="flex gap-5">
+        <div className="flex gap-5">
         <a
           href="https://www.linkedin.com/in/oddvarzk"
           target="_blank"
@@ -44,6 +29,19 @@ export function Footer() {
         >
           <img src={github} className="h-8" alt="GitHub" />
         </a>
+      </div>
+      </div>
+
+      {/* Contact Information */}
+      <div className="flex flex-col items-center gap-2 text-center font-Montserrat">
+        <a
+          href="mailto:your-email@example.com"
+          className="hover:underline text-sm"
+        >
+          oddvarzk@gmail.com
+        </a>
+        <p className="font-Montserrat text-sm">Phone: +47 472 07 143</p>
+        
       </div>
     </footer>
   );
