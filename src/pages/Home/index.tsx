63cb3f62b4c6d02b9ebbea8a6d0339fb3b868c23
oddvar zakarias kristiansen <oddvarzk@gmail.com>
2024-12-11 07:46:10 +0100

    Layout of homepage fixed, now mobile friendly

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,49 @@
+import workPicture from "../../assets/pictureMe.jpeg";
+import viteSVG from "../../assets/viteIcon.svg";
+
 export function Home() {
   return (
-    <div className="mt-5">
-      <div className="flex justify-center text-Cream gap-10 font-Montserrat">
-        <div>
-          <h1 className="text-2xl">Hi!</h1>
-          <h2 className="text-lg py-2">Welcome to my Portofolio</h2>
-          <p className="w-1/2">
-            I'm a newly graduated Front-end Developer, this site is made so you
-            can get to know me better, but also see what skills i have to offer
-            with the wide range of projects i have created.
+    <div className="mt-5 px-5 py-3">
+      <div
+        id="home"
+        className="flex flex-col md:flex-row justify-center text-Cream gap-10 font-Montserrat items-center"
+      >
+        <div className="w-full md:w-1/2">
+          <h2 className="text-lg py-2 font-light">
+            Hi there! My name is Zakarias, welcome to my one and only portfolio
+            website.
+          </h2>
+          <p className="font-light py-2">
+            I'm a newly graduated Front-end Developer. With a solid interest
+            tech, new technology and becoming a senior full-stack developer in
+            the upcomming future.
           </p>
-          <div className="flex">
-            <button className="bg-Cream text-black px-3 py-1 mt-5">
-              See more projects
+          <p className="font-light py-2">
+            This website is for you, so that you can get to know me better and
+            see the skills I have to offer through the wide range of projects
+            I've created.
+          </p>
+          <div className="flex justify-center py-3">
+            <p>This website is made with:</p>
+            <div className="flex">
+              <img src={viteSVG} alt="" />
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <button className="bg-Burgundy text-white px-3 py-1 mt-5 hover:bg-opacity-50 transition">
+              Go to my projects
             </button>
           </div>
         </div>
-        <div className="font-Montserrat">
-          <p>Image</p>
+        <div className="w-full md:w-1/2">
+          <img
+            className="w-full h-auto md:h-96 object-cover rounded"
+            src={workPicture}
+            alt="Picture of me in the mountain"
+          />
         </div>
       </div>
+      {/* add more content here */}
     </div>
   );
 }
