ea4a6cf05872f90d220de7cf30947ea11f6dc28a
oddvar zakarias kristiansen <oddvarzk@gmail.com>
2024-12-11 07:44:51 +0100

    new color and font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,12 @@ export default {
       colors: {
         Cream: "#F8F4E3",
         Charcoal: "#1a1a1a",
+        Burgundy: "#556B2F",
       },
       fontFamily: {
         Montserrat: ["Montserrat", "sans-serif"], // for body text
         Playfair: ["Playfair Display", "serif"], // for headings
+        Bebas: ["Bebas Neue", "serif"],
       },
     },
   },
