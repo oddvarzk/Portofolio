2a653c0d87aca4058ad79726eca91eed4f4e5fc1
oddvar zakarias kristiansen <oddvarzk@gmail.com>
2024-12-15 02:54:21 +0100

    AOS need setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./layout";
 import Home from "./pages/Home";
+// import AOS from 'aos';
+import "aos/dist/aos.css";
 
 function App() {
   return (
